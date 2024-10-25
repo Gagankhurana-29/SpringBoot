e4b5a7f123523609431a3538e31c6399ec5fcbac
GaganKhurana_29 <gagankhurana616@gmail.com>
2024-10-25 23:38:02 +0530

    ResponseEntity controller updates

--- src/main/java/com/example/introductionToSpringBoot/department/controllers/DepartmentController.java
+++ src/main/java/com/example/introductionToSpringBoot/department/controllers/DepartmentController.java
@@ -4,9 +4,12 @@ import com.example.introductionToSpringBoot.department.DTO.DepartmentDTO;
 import com.example.introductionToSpringBoot.department.Entity.DepartmentEntity;
 import com.example.introductionToSpringBoot.department.Repositories.DepartmentRepository;
 import com.example.introductionToSpringBoot.department.Service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/departments")
@@ -19,20 +22,37 @@ class DepartmentController {
     }
 
     @GetMapping(path = "/getAllDepartments/")
-    public List<DepartmentDTO> getDepartments(){
+    public ResponseEntity<List<DepartmentDTO>> getDepartments(){
         System.out.println("Getting the departments");
-        return departmentService.findAll();
+        List<DepartmentDTO> allDepartments =  departmentService.findAll();
+        return ResponseEntity.ok(allDepartments);
     }
 
     @GetMapping(path = "/{departmentID}")
-    public String getDepartMentByID(@PathVariable(name = "departmentID") int id){
+    public String getDepartmentByID(@PathVariable(name = "departmentID") int id){
         return "Department fetched with id " + id;
     }
 
     @PostMapping(path = "/addDepartments/")
-    public DepartmentDTO addDepartment(@RequestBody DepartmentDTO departmentDTO){
+    public ResponseEntity<DepartmentDTO> addDepartment(@RequestBody DepartmentDTO departmentDTO){
         //departmentEntity.setName("IT Department");
-        return departmentService.save(departmentDTO);
+        DepartmentDTO savedDepartment =  departmentService.save(departmentDTO);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(path = "/{departmentID}")
+    public ResponseEntity<DepartmentDTO> updateDepartment(@RequestBody Map<String, Object> updates,
+                                          @PathVariable int departmentID ){
+        DepartmentDTO updatedDepartment = departmentService.updateDepartment(departmentID,updates);
+        if(updatedDepartment == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(updatedDepartment);
+    }
+
+    @DeleteMapping(path = "/{departmentId}")
+    public ResponseEntity<Boolean> deleteDepartment(@PathVariable int departmentId){
+         Boolean isDeleted = departmentService.deleteDepartment(departmentId);
+         if(!isDeleted) return ResponseEntity.notFound().build();
+          return ResponseEntity.ok(true);
     }
 
 }
