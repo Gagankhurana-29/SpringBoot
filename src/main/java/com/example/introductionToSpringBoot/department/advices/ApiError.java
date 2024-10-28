9e1093012330b62f8cdc27e79b02bddd9305d431
GaganKhurana_29 <gagankhurana616@gmail.com>
2024-10-28 23:27:39 +0530

    Brushing Response

--- src/main/java/com/example/introductionToSpringBoot/department/advices/ApiError.java
+++ src/main/java/com/example/introductionToSpringBoot/department/advices/ApiError.java
@@ -2,11 +2,15 @@ package com.example.introductionToSpringBoot.department.advices;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
 @Data
+@Getter
+@Setter
 public class ApiError {
     private HttpStatus status;
     private String message;
@@ -20,6 +24,30 @@ public class ApiError {
         this.status = status;
         this.message = message;
          this.subErrors = subErrors;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getSubErrors() {
+        return subErrors;
+    }
+
+    public void setSubErrors(List<String> subErrors) {
+        this.subErrors = subErrors;
     }
 
     private List<String> subErrors;
